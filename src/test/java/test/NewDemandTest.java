edd0ae76fb4f1867d71c45c05df9af735c28c84a
Kirandeshmukhpatil93 <kirandeshmukhpatil93@gmail.com>
2025-06-12 14:43:07 +0530

    new test case added

--- src/test/java/test/NewDemandTest.java
+++ src/test/java/test/NewDemandTest.java
@@ -9,7 +9,7 @@ import utility.ExcelUtil;
 public class NewDemandTest extends BaseTest{
 	 public static String testDataFileLocation= System.getProperty("user.dir")+"\\TestData\\NewDemand.xlsx";
 	
-	 @Test(priority=1,description="create new demand for HR Department")
+	 @Test(priority=1,enabled=false,description="create new demand for HR Department")
 	 public void CreateNewDemand_HR() throws IOException, InterruptedException {
 		testData = new ExcelUtil(testDataFileLocation,"Sheet1");
 	      String dropDepartment = testData.getExcelAsMap().get(0).get("department");
@@ -36,12 +36,37 @@ public class NewDemandTest extends BaseTest{
 	      demand.clickOnSubmitBtn();
 	      demand.verify_demandCreation();
 	      
-	      
-	      
-	      
 	}
 	
 	
+	 @Test(priority=2,enabled=true,description="create new demand for QA Department")
+	 public void CreateNewDemand_QA() throws IOException, InterruptedException {
+		testData = new ExcelUtil(testDataFileLocation,"Sheet1");
+	      String dropDepartment = testData.getExcelAsMap().get(1).get("department");
+	      String dropDesignation = testData.getExcelAsMap().get(1).get("designation");
+	      String  countPosition = testData.getExcelAsMap().get(1).get("positions");
+	      String  dropJobType = testData.getExcelAsMap().get(1).get("jobtype");
+	      String  dropCountry = testData.getExcelAsMap().get(1).get("country");
+	      String  dropCity = testData.getExcelAsMap().get(1).get("city");
+	      String   description = testData.getExcelAsMap().get(1).get("jobDiscription");
+	      String dropPriority =testData.getExcelAsMap().get(1).get("priority");
+	      
+	      
+	      homePage.clickOnCreateDemandAction();    
+	      demand.select_Department(dropDepartment);
+	      demand.select_Designation(dropDesignation);
+	      demand.enter_NoOfPositions(countPosition);
+	      demand.select_JobType(dropJobType);
+	      demand.select_Country(dropCountry);
+	      demand.upload_JobDescription(description);
+	      demand.select_Priority(dropPriority);
+	      demand.select_City(dropCity);
+	      demand.click_NextBtn();
+	      demand.fillCriteria();
+	      demand.clickOnSubmitBtn();
+	      demand.verify_demandCreation();
+	      
+	}
 	
 	
 	
